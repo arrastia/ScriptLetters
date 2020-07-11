73855fda2caed9eb07e04966ba85b9daae2fe91b
Arrastia <arrastia@pm.me>
2020-07-11 20:05:27 +0200

    add Home view

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { Home } from 'views/Home';
+
+export const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </Router>
+);
